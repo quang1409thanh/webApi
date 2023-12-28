a9dc23d770e51a3c67f1a97e79ced2725d2231b5
L2Nam <lenamlhn234@gmail.com>
2023-12-28 14:20:57 +0700

    aggregation manager, employee

--- fe/src/components/Users/AggregationHead/AggregationHead.jsx
+++ fe/src/components/Users/AggregationHead/AggregationHead.jsx
@@ -1,11 +1,10 @@
-import {Fragment} from "react";
-import {Navigate, NavLink, Outlet} from "react-router-dom";
+import { Fragment } from "react";
+import { Navigate, NavLink, Outlet } from "react-router-dom";
 import Footer from "../../Common/Footer.jsx";
 export default function AggregationHead() {
     return (
         <>
-            <Outlet/>
-            <Footer/>
+            <Outlet />
         </>
     );
 }
